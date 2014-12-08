5a70c1337d36654e3dcb870c7e443b4ebe199871
Shawn Brandt <djbigdad@djbigdad.local>
2014-12-08 16:20:36 -0600

    cpkdev v1.0.6.8.31

--- public/javascripts/basic.js
+++ public/javascripts/basic.js
@@ -954,6 +954,22 @@ window.addEventListener("load",function() {
 	}, 0);
 });
 
+//smooth scrolling on back to top button
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1250);
+        return false;
+      }
+    }
+  });
+});
+
 
 
 
