b7595e96799294a1260349807dcdca5154d5153f
Shawn Brandt <djbigdad@djbigdad.local>
2014-12-08 16:43:41 -0600

    cpkdev v1.0.6.8.35

--- public/javascripts/basic.js
+++ public/javascripts/basic.js
@@ -955,20 +955,20 @@ window.addEventListener("load",function() {
 });
 
 //smooth scrolling on back to top button
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1250);
-        return false;
-      }
-    }
-  });
-});
+// $(function() {
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1250);
+//         return false;
+//       }
+//     }
+//   });
+// });
 
 
 
